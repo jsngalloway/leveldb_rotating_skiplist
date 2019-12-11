16503e8048398d16a475e2a1a8075de767f3ba63
Jesse Galloway <jsg220@lehigh.edu>
2019-12-11 17:42:32 -0500

    created levelDB  skiplist isolation test files

--- tests/leveldb/timed_test/db_test.cpp
+++ tests/leveldb/timed_test/db_test.cpp
@@ -33,7 +33,7 @@ string lookup(leveldb::DB* db, string key){
         return value;
 }
 
-bool remove(leveldb::DB* db, string key, string value){
+bool remove(leveldb::DB* db, string key){
     return(db->Delete(leveldb::WriteOptions(), key)).ok();
 }
 
@@ -46,7 +46,7 @@ int main()
     assert(status.ok());
     cout << "Created databse.\t\t\t\tStatus: " << str_status(status.ok()) << endl;
 
-    size_t OPERATIONS = 100;
+    size_t OPERATIONS = 100000;
     float percent_insert = 0.2;
     float percent_lookup = 0.7;
     float percent_delete = 0.1;
@@ -56,46 +56,57 @@ int main()
     for(size_t n = OPERATIONS; n > 0; n--){
         float r = ((double) rand() / (RAND_MAX));
         if (r < percent_insert) {
+
             //insert
             string val = "DOG";//std::to_string((char)(rand()%40));
-            if(!insert(db, std::to_string(n), val)){
+            if(!insert(db, std::to_string(n+1), val)){
                 cout << "INSERT ERROR" << endl;
             } else {
-                inserted_values.push_back(std::to_string(n));
+                inserted_values.push_back(std::to_string(n+1));
             }
-            cout << "inserting: (" << std::to_string(n) << ", " << val << ")."<<endl;
-        } else if ((r + percent_insert) < percent_lookup) {
+            // cout << "inserting: (" << std::to_string(n+1) << ", " << val << ")."<<endl;
+
+
+
+        } else if (r < percent_insert + percent_lookup) {
             //lookup
-            cout << "conducting lookup " << endl;
-            int k_len = inserted_values.size();
-            int index = rand() % k_len;
-            std::list<std::string>::iterator it = inserted_values.begin();
-            std::advance(it, index);
-            string lookup_key  = *it;
-            // string lookup_key = inserted_values[index];
-            // cout << "Looking up key: " << lookup_key << endl;
-            if(lookup(db, lookup_key).empty()){
-                cout << "Lookup error" << endl;
+            if(!inserted_values.empty()){
+                int k_len = inserted_values.size();
+                int index = rand() % k_len;
+                std::list<std::string>::iterator it = inserted_values.begin();
+                std::advance(it, index);
+                string lookup_key  = *it;
+                
+                // cout << "Looking up key: " << lookup_key;
+                if(lookup(db, lookup_key).empty()){
+                    cout << "Lookup error" << endl;
+                } else {
+                    // cout << " Success" << endl;
+                }
             }
-        } else if ((r + percent_insert + percent_lookup) < percent_delete){
-            //delete
-            // cout << "conducting delete " << endl;
-            // int k_len = inserted_values.size();
-            // int index = rand() % k_len;
-            // std::list<std::string>::iterator it = inserted_values.begin();
-            // std::advance(it, index);
-            // string lookup_key  = *it;
-            // // string lookup_key = inserted_values[index];
-            // // cout << "Looking up key: " << lookup_key << endl;
-            // if(lookup(db, lookup_key).empty()){
-            //     cout << "Lookup error" << endl;
-            // }
+        } else if (r < percent_insert + percent_lookup + percent_delete){
+            // delete
+            if(!inserted_values.empty()){
+                int k_len = inserted_values.size();
+                int index = rand() % k_len;
+                std::list<std::string>::iterator it = inserted_values.begin();
+                std::advance(it, index);
+                string lookup_key  = *it;
+                // string lookup_key = inserted_values[index];
+                // cout << "deleting key: " << lookup_key << endl;
+                if(!remove(db, lookup_key)){
+                    cout << "Delete error" << endl;
+                } else {
+                    // cout << " Success" << endl;
+                    inserted_values.erase(it);
+                }
+            }
         }
     }
     chrono::high_resolution_clock::time_point end = chrono::high_resolution_clock::now();
     chrono::duration<double> elapsed_time = chrono::duration_cast<chrono::duration<double>>(end - start);
 
-    std::cout << "It took me " << elapsed_time.count()*1000000 << " microseconds.";
+    std::cout << "It took me " << elapsed_time.count()*1000 << "ms (" << elapsed_time.count() << " seconds)" << endl;
 
 
     delete db;
