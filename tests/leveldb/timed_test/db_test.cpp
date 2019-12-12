74fb0ad2d2ae19a676da146f05ce098e46f0c2fe
Jesse Galloway <jsg220@lehigh.edu>
2019-12-12 13:10:52 -0500

    created comprable tests

--- tests/leveldb/timed_test/db_test.cpp
+++ tests/leveldb/timed_test/db_test.cpp
@@ -46,28 +46,41 @@ int main()
     assert(status.ok());
     cout << "Created databse.\t\t\t\tStatus: " << str_status(status.ok()) << endl;
 
-    size_t OPERATIONS = 100000;
-    float percent_insert = 0.2;
-    float percent_lookup = 0.7;
-    float percent_delete = 0.1;
+    double TEST_DURATION_S = 10;
+    size_t pre_population_size = 256;
+    float percent_insert = 0.00;
+    float percent_lookup = 0.95;
+    float percent_delete = 0.00;
+    float percent_updates = 0.05;
     list<string> inserted_values;
-    srand(time(0)); 
+
+    for(size_t i = pre_population_size; i > 0; i--){
+        string val = "DOG";
+        if(!insert(db, std::to_string(i+1), val)){
+            cout << "(PRE)INSERT ERROR" << endl;
+        } else {
+            inserted_values.push_back(std::to_string(i+1));
+        }
+    }
+
+    srand(time(0));
     chrono::high_resolution_clock::time_point start = chrono::high_resolution_clock::now();
-    for(size_t n = OPERATIONS; n > 0; n--){
+    size_t n = pre_population_size;
+    size_t ins = 0;
+    size_t lkup = 0;
+    size_t del = 0;
+    while(chrono::duration_cast<chrono::duration<double>>(chrono::high_resolution_clock::now() - start).count() < TEST_DURATION_S){
         float r = ((double) rand() / (RAND_MAX));
         if (r < percent_insert) {
-
             //insert
             string val = "DOG";//std::to_string((char)(rand()%40));
             if(!insert(db, std::to_string(n+1), val)){
                 cout << "INSERT ERROR" << endl;
             } else {
                 inserted_values.push_back(std::to_string(n+1));
+                ins++;
             }
             // cout << "inserting: (" << std::to_string(n+1) << ", " << val << ")."<<endl;
-
-
-
         } else if (r < percent_insert + percent_lookup) {
             //lookup
             if(!inserted_values.empty()){
@@ -82,6 +95,7 @@ int main()
                     cout << "Lookup error" << endl;
                 } else {
                     // cout << " Success" << endl;
+                    lkup++;
                 }
             }
         } else if (r < percent_insert + percent_lookup + percent_delete){
@@ -99,16 +113,23 @@ int main()
                 } else {
                     // cout << " Success" << endl;
                     inserted_values.erase(it);
+                    del++;
                 }
             }
         }
+        n++;
+        // end = chrono::high_resolution_clock::now();
+        // chrono::duration<double> elapsed_time = chrono::duration_cast<chrono::duration<double>>(chrono::high_resolution_clock::now() - start);
+
     }
     chrono::high_resolution_clock::time_point end = chrono::high_resolution_clock::now();
     chrono::duration<double> elapsed_time = chrono::duration_cast<chrono::duration<double>>(end - start);
 
     std::cout << "It took me " << elapsed_time.count()*1000 << "ms (" << elapsed_time.count() << " seconds)" << endl;
-
-
+    cout << "COMPLETED: " << (n - pre_population_size) << " (" << (n - pre_population_size) - (del + ins + lkup) << " failed)" << endl;
+    cout << "Inserts: " << ins << endl;
+    cout << "Lookup: " << lkup << endl;
+    cout << "Delete: " << del << endl;
     delete db;
     cout << "Completed test." << endl;
 
